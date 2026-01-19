58a84a09fbfb0e5362adbcecb0b99f24885dd1bc
nasnasrich <nasnasrich@gmail.com>
2026-01-18 23:48:28 -0800

    Fix auth middleware and register route

--- server.js
+++ server.js
@@ -1,28 +1,4 @@
- /*import dotenv from 'dotenv';
-import mongoose from 'mongoose'
-import express from "express";
-import userRoute from "./route/user.js"
-//const { default: mongoose } = require('mongoose');
 
-const server = express()
-server.use(express.json());
-dotenv.config()
-server.listen(5000,()=>{
-  console.log(server is running in port${process.env.PORT})
-})
-//Routes
-server.use('api/users',userRoute)
-
-console.log("my Name is OCJ")
-
-//server.get('/',(req,res)=>{
-  //res.send('Hello world')
-//})
-mongoose.connect(process.env.MONGO_URL).then(()=>{
-  console.log("connection successful")
-}).catch(()=>{
-  console.log('not connected')
-})*/
 
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
